24e550f9c03c400acc186a566d3f21197d97e55d
ArtyomHakobyan <aghakobyan@gmail.com>
2024-12-24 03:25:53 -0800

    Added ConsumerProducer solution with CondVars (C++)

---
+++ Concurrency/ConditionVariables/ConsumerProducer.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <queue>
+#include <random>
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+int main()
+{
+    std::queue<int> data{};
+    std::mutex data_mutex{};
+    std::condition_variable not_full_cv{};
+    std::condition_variable not_empty_cv{};
+
+    const int max_data_size = 5;
+
+    auto produce_function = [&](){
+        while (true)
+        {
+            std::unique_lock<std::mutex> lock(data_mutex);
+            not_full_cv.wait(lock, [&data](){ return data.size() < max_data_size; });
+
+            int item = std::rand() % 10 + 1;
+            data.push(item);
+            std::cout << "Produced " << std::to_string(item) << ", container size: " << data.size()<< std::endl;
+
+            not_empty_cv.notify_one();
+        }
+    };
+    auto consume_function = [&](){
+        while (true)
+        {
+            std::unique_lock<std::mutex> lock(data_mutex);
+            not_empty_cv.wait(lock, [&data](){ return !data.empty() ;});
+
+            auto var = data.front();
+            data.pop();
+            std::cout << "Consumed " << std::to_string(var) << ", container size: " << data.size()<< std::endl;
+
+            not_full_cv.notify_one();
+        }
+    };
+
+    auto producer_thread = std::thread(produce_function);
+    auto consumer_thread = std::thread(consume_function);
+
+    if (producer_thread.joinable())
+        producer_thread.join();
+    if (consumer_thread.joinable())
+        consumer_thread.join();
+
+    return 0;
+}
