ed4049b521cead7c2bc36c5cfed5757c49cb3c76
tyomhak <aghakobyan@gmail.com>
2025-01-22 05:50:48 +0400

    Fix Fibonacci

--- Algs/DP/Fibonacci.cpp
+++ Algs/DP/Fibonacci.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <cmath>
+#include <vector>
 
 int fib_l(int n, std::map<int, int>& pre_calced)
 {
@@ -12,7 +13,7 @@ int fib_l(int n, std::map<int, int>& pre_calced)
             pre_calced[N] = fib_l(N, pre_calced);
     };
 
-    if (n % 2 == 0)
+    if (n % 2 != 0)
     {
         pre_calc(n / 2);
         pre_calc(n / 2 + 1);
@@ -32,9 +33,9 @@ int fib_l(int n, std::map<int, int>& pre_calced)
 int fib_l(int n)
 {
     std::map<int,int> precalced{};
-    precalced[0] = 1;
+    precalced[0] = 0;
     precalced[1] = 1;
-    for (int i = 2; i < 10; ++i)
+    for (int i = 2; i < 6; ++i)
         precalced[i] = precalced[i - 1] + precalced[i - 2];
 
     return fib_l(n, precalced);
@@ -42,8 +43,22 @@ int fib_l(int n)
 
 int main()
 {
-    for (int i = 0; i < 20; ++i)
-        std::cout << fib_l(i) << ", ";
+    std::vector<int> fib_nums = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181};
+    std::vector<int> my_fib_nums{};
+
+    for (int i = 0; i < fib_nums.size(); ++i)
+        my_fib_nums.push_back(fib_l(i));
     
-    std::cout << std::endl;
+    for (int i = 0; i < fib_nums.size(); ++i)
+        std::cout << fib_nums[i] << ", ";
+    std::cout << "\n";
+    for (int i = 0; i < my_fib_nums.size(); ++i)
+        std::cout << my_fib_nums[i] << ", ";
+    std::cout << "\n";
+
+    for (int i = 0; i < fib_nums.size(); ++i)
+    {
+        if (fib_nums[i] != my_fib_nums[i])
+            std::cout << "Wrong\n";
+    }
 }
