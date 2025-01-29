0c780f5d1b7bdd2a456a3568bca971a85eb2fe0d
tyomhak <aghakobyan@gmail.com>
2025-01-29 14:45:22 +0400

    Fix LCS memo. Add computed % calculation.

--- Algs/LCS/LCS.cpp
+++ Algs/LCS/LCS.cpp
@@ -4,18 +4,20 @@
 
 
 template<class T>
-int lcs_memo(const std::vector<T>& l, const std::vector<T>& r, std::vector<std::vector<int>>& cache, int l_ndx, int r_ndx)
+int lcs_memo(const std::vector<T>& l, const std::vector<T>& r, std::vector<std::vector<int>>& cache, int l_ndx, int r_ndx, int &computed_count)
 {
     if (cache[l_ndx][r_ndx] == -1)
     {
-        if (l[l_ndx] == r[r_ndx])
-            cache[l_ndx][r_ndx] = lcs_memo(l, r, cache, l_ndx - 1, r_ndx - 1) + 1;
+        if (l[l_ndx - 1] == r[r_ndx - 1])
+            cache[l_ndx][r_ndx] = lcs_memo(l, r, cache, l_ndx - 1, r_ndx - 1, computed_count) + 1;
         else
         {
             cache[l_ndx][r_ndx] = std::max(
-                lcs_memo(l, r, cache, l_ndx - 1, r_ndx),
-                lcs_memo(l, r, cache, l_ndx, r_ndx - 1));
+                lcs_memo(l, r, cache, l_ndx - 1, r_ndx, computed_count),
+                lcs_memo(l, r, cache, l_ndx, r_ndx - 1, computed_count));
         }
+
+        computed_count++;
     }
 
     return cache[l_ndx][r_ndx];
@@ -24,13 +26,17 @@ int lcs_memo(const std::vector<T>& l, const std::vector<T>& r, std::vector<std::
 template<class T>
 int lcs_memo(const std::vector<T>& l, const std::vector<T>& r)
 {
+    int computed_count = 0;
     std::vector<std::vector<int>> cache(l.size() + 1, std::vector<int>(r.size() + 1, -1));
     for (int i = 0; i < cache.size(); ++i)
         cache[i][0] = 0;
     for (int i = 0; i < cache[0].size(); ++i)
         cache[0][i] = 0;
 
-    return lcs_memo(l, r, cache, l.size(), r.size());
+    auto result = lcs_memo(l, r, cache, l.size(), r.size(), computed_count);
+    std::cout << "Computed " << (float)computed_count * 100.0f / (float)(l.size() * r.size()) << "% of total." << std::endl;
+
+    return result;
 }
 
 template<class T>
@@ -84,13 +90,15 @@ std::vector<T> lcs_dp_items(const std::vector<T>& l, const std::vector<T>& r)
 
 int main()
 {
-    std::vector<char> l{'c', 't', 'g', 'a', 'a', 't'};
-    std::vector<char> r{'a', 'c', 'a', 't', 'g', 't'};
+    std::vector<char> l{'c', 't', 'g', 'a', 'a', 't', 'g', 'c', 'a', 't', 'g', 'c', 'a', 't', 'g', 'a', 'c', 't', 'g', 'a', 'c', 'g', 't', 'a', 'c', 'g', 't', 'a', 'c', 'g', 't', 'a', 'c', 'g', 't', 'a', 'c', 'g', 't', 'a'};
+    std::vector<char> r{'a', 'c', 'a', 't', 'g', 't', 'c', 'a', 't', 'g', 'c', 'a', 't', 'g', 'a', 'c', 't', 'g', 'a', 't', 'c', 'g', 'a', 't', 'c', 'g', 'a', 't', 'c', 'g', 'a', 't', 'c', 'g', 'a', 't', 'c', 'g', 'a', 't'};
 
-    std::cout << "memo LCS length: " << lcs_memo(l, r) << std::endl;
-    auto result_lcs = lcs_dp_items(l, r);
-    std::cout << "dp LCS length: " << result_lcs.size() << "\n";
-    for (auto c : result_lcs)
+    auto lcs_result_memo = lcs_memo(l, r);
+    std::cout << "memo LCS length: " << lcs_result_memo << std::endl;
+
+    auto lcs_result_dp = lcs_dp_items(l, r);
+    std::cout << "dp LCS length: " << lcs_result_dp.size() << "\n";
+    for (auto c : lcs_result_dp)
         std::cout << c;
     std::cout << std::endl;
 }
