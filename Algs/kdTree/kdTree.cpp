11b8b860340f61a95260dd84323ccfb29ad5a584
tyomhak <aghakobyan@gmail.com>
2025-02-05 21:03:27 +0400

    Add K-NNSearch to kdTree

--- Algs/kdTree/kdTree.cpp
+++ Algs/kdTree/kdTree.cpp
@@ -1,19 +1,14 @@
 
 #include <iostream>
-#include <vector>
-#include <memory>
 #include <cstdlib>
 
 
-using Point = std::vector<int>;
+#include <vector>
+#include <queue>
+#include <memory>
+#include <cmath>
 
-// void print(const Point& point)
-// {
-//     std::cout << "(";
-//     for (auto it = point.begin(); it != (point.end() - 1); ++it)
-//         std::cout << *it << ",";
-//     std::cout << *point.rbegin() << ")";
-// }
+using Point = std::vector<int>;
 
 std::ostream& operator<<(std::ostream& os, const Point& point) 
 {
@@ -21,6 +16,26 @@ std::ostream& operator<<(std::ostream& os, const Point& point)
     for (auto it = point.begin(); it != (point.end() - 1); ++it)
         os << *it << ",";
     return os << *point.rbegin() << ")";
+}
+
+int square_distance(const Point& l, const Point& r)
+{
+    int result = 0;
+    for (int dimention = 0; dimention < l.size(); ++dimention)
+    {
+        result += (l[dimention] - r[dimention]) * (l[dimention] - r[dimention]);
+    }
+    return result;
+}
+
+int dimention_distance(const Point& l, const Point& r, int dimention)
+{
+    return abs(l.at(dimention) - r.at(dimention));
+}
+
+int smaller(const Point& l, const Point& r, int dimention)
+{
+    return l[dimention] < r[dimention];
 }
 
 struct Node
@@ -40,6 +55,18 @@ struct Node
 
 class KD_Tree
 {
+struct SquareDistComparator
+{
+    SquareDistComparator(const Point& target_point) : target(target_point) {}
+
+    bool operator()(const Point& l, const Point& r)
+    {
+        return square_distance(target, l) < square_distance(target, r);
+    }
+
+    Point target{};
+};
+
 public:
 
     void insert(const Point& point)
@@ -67,6 +94,22 @@ public:
         return result_point;
     }
 
+    std::vector<Point> NNSearch(const Point& target_point, int n)
+    {
+        SquareDistComparator cmp(target_point);
+        std::priority_queue<Point, std::vector<Point>, SquareDistComparator> pq(cmp);
+        NNSearch(target_point, _head.get(), pq, n, 0);
+
+        std::vector<Point> results{};
+        results.reserve(pq.size());
+        while (!pq.empty())
+        {
+            results.push_back(pq.top());
+            pq.pop();
+        }
+        return results;
+    }
+
     void debug_print()
     {
         if (!_head) return;
@@ -75,6 +118,35 @@ public:
 
 
 private:
+    void NNSearch(const Point& point, const Node* head, std::priority_queue<Point, std::vector<Point>, SquareDistComparator>& pq, int n, int dimention)
+    {
+         if (!head) return;
+
+        if (head->point.at(2) == 23)
+            bool debug_stop = true;
+
+        pq.push(head->point);
+        while (pq.size() > n)
+            pq.pop();
+
+        bool is_left = smaller(head->point, point, dimention);
+        if (is_left)
+            NNSearch(point, head->l.get(), pq, n, (dimention + 1) % point.size());
+        else
+            NNSearch(point, head->r.get(), pq, n, (dimention + 1) % point.size());
+
+        if (is_left && head->r)
+        {
+            if (auto dim_dist = dimention_distance(head->r->point, point, dimention); sqrt(square_distance(pq.top(), point)) > (double)dim_dist || pq.size() < n)
+                NNSearch(point, head->r.get(), pq, n, (dimention + 1) % point.size());
+        }
+        else if (!is_left && head->l)
+        {
+            if (auto dim_dist = dimention_distance(head->l->point, point, dimention); sqrt(square_distance(pq.top(), point)) > (double)dim_dist || pq.size() < n)
+                NNSearch(point, head->l.get(), pq, n, (dimention + 1) % point.size());
+        }
+    }
+
     void NNSearch(const Point& point, const Node* head, int& min_sqr_distance, Point& closestPoint, int dimention)
     {
         if (!head) return;
@@ -104,25 +176,7 @@ private:
         }
     }
 
-    int square_distance(const Point& l, const Point& r)
-    {
-        int result = 0;
-        for (int dimention = 0; dimention < l.size(); ++dimention)
-        {
-            result += (l[dimention] - r[dimention]) * (l[dimention] - r[dimention]);
-        }
-        return result;
-    }
-
-    int dimention_distance(const Point& l, const Point& r, int dimention)
-    {
-        return abs(l.at(dimention) - r.at(dimention));
-    }
-
-    int smaller(const Point& l, const Point& r, int dimention)
-    {
-        return l[dimention] < r[dimention];
-    }
+    
 
     void insert(Node* head, const Point& point, int dimention)
     {
@@ -171,8 +225,10 @@ int main()
     for (int i = 0; i < 10; ++i)
     {
         Point pt{ rand() % 30, rand() % 30, rand() % 30};
+        std::cout << pt << ", ";
         tree.insert(pt);
     }
+    std::cout << std::endl;
 
     tree.debug_print();
 
@@ -188,4 +244,13 @@ int main()
 
     for (auto target : targets)
         std::cout << "Closest to " << target << " is " << tree.NNSearch(target) << std::endl;
+
+    for (auto target : targets)
+    {
+        std::cout << "Closest to " << target << " are:" << std::endl;
+        auto closest_n = tree.NNSearch(target, 5);
+        for (auto point : closest_n)
+            std::cout << "    " << point << "\n";
+        std::cout << "\n";
+    }
 }
